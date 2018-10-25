1a9efa5804588aec564ccaa8fea7ec6a24aa377f
Sai Keerthana <kmakalas@xilinx.com>
2018-10-25 03:47:55 -0400

    MISRA-C

---
+++ MISRA-C_reports/LDRA Testbed Code Review Report_files/imagejs.js
@@ -0,0 +1,9 @@
+var temp_str ='<img src="file:///proj/xhdsswstaff/ssw/tools/LDRA/Non-Safety/ldra_toolsuite954/images/';
+var base_str ='<a href="file:///proj/xhdsswstaff/ssw/tools/LDRA/Non-Safety/ldra_toolsuite954/';
+function TBLaunchit(eParam, eOtherParam)
+{
+var RunLine;
+RunLine=  '"' + '/proj/xhdsswstaff/ssw/tools/LDRA/Non-Safety/ldra_toolsuite954/tbbrowse' + '"| '
+ + '"' + eParam + '"'   + '-Start=' + eOtherParam + ' ';
+LDRATBLaunch.ExecuteIt(RunLine);
+}
